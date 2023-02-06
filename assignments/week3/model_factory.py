8a3b69279a18ff3c88a047b0eabf32527fc58435
YichengShen <jsszsyc@hotmail.com>
2023-02-06 18:10:40 -0500

    update

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -18,7 +18,7 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
 
     return MLP(
         input_dim,
-        206,
+        208,
         output_dim,
         2,
         torch.nn.LeakyReLU,
