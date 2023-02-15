c3e17e984f7e8897ca71d984080d3b512b25cb9d
YichengShen <jsszsyc@hotmail.com>
2023-02-15 10:43:18 -0500

    Update

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -12,16 +12,12 @@ class CustomLRScheduler(_LRScheduler):
         self,
         optimizer,
         decay_rate=0.999,
-        decay_epochs=200,
+        decay_epochs=20,
         last_epoch=-1,
         verbose=False,
     ):
         """
         Create a new scheduler.
-
-        Note to students: You can change the arguments to this constructor,
-        if you need to add new parameters.
-
         """
 
         self.decay_rate = decay_rate
@@ -32,7 +28,6 @@ class CustomLRScheduler(_LRScheduler):
         """
         Returns a list of learning rates.
         """
-
         if self.last_epoch % self.decay_epochs != 0:
             return [group["lr"] for group in self.optimizer.param_groups]
         return [group["lr"] * self.decay_rate for group in self.optimizer.param_groups]
