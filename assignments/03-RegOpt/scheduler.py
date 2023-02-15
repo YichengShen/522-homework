c04a309804c956c679a6891b9cca24953f3dd481
YichengShen <jsszsyc@hotmail.com>
2023-02-15 12:09:35 -0500

    Update

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -32,7 +32,7 @@ class CustomLRScheduler(_LRScheduler):
         last_epoch (int): The index of the last epoch.
     """
 
-    def __init__(self, optimizer, T_0=10, T_mult=1.5, eta_min=0.005, last_epoch=-1):
+    def __init__(self, optimizer, T_0=1000, T_mult=2, eta_min=0.000006, last_epoch=-1):
         """
         Create a new scheduler.
         """
