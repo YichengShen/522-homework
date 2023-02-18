834db34950d493a686feaa66b367591f4d875af1
YichengShen <jsszsyc@hotmail.com>
2023-02-18 10:20:25 -0500

    Update

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -88,7 +88,7 @@ class CustomLRScheduler(_LRScheduler):
         self,
         optimizer,
         decay_rate=0.999,
-        decay_epochs=12,
+        decay_epochs=30,
         last_epoch=-1,
         verbose=False,
     ):
