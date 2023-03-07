e8ecf275a618b57f90a44143072e9c6f54b84eda
YichengShen <jsszsyc@hotmail.com>
2023-03-07 12:27:32 -0500

    Update

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,7 +6,7 @@ from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
-    batch_size = 160
+    batch_size = 150
     num_epochs = 10
 
     optimizer_factory: Callable[
