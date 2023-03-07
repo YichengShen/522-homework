4fb5c124a616a9405c0f7cd594f85cfc1c9c94de
YichengShen <jsszsyc@hotmail.com>
2023-03-06 22:27:22 -0500

    Update

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,41 @@
 import torch
+import torch.nn as nn
 
 
 class Model(torch.nn.Module):
+    """
+    A simple CNN.
+    """
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=num_channels,
+            out_channels=16,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(
+            in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1
+        )
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(in_features=32 * 8 * 8, out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        """
+        Forward.
+        """
+        x = self.conv1(x)
+        x = nn.functional.relu(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = nn.functional.relu(x)
+        x = self.pool2(x)
+        x = x.view(-1, 32 * 8 * 8)
+        x = self.fc1(x)
+        x = nn.functional.relu(x)
+        x = self.fc2(x)
+        return x
