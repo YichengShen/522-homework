881c0f9c47bb1e94adb5f5f688d8665caaa0c200
YichengShen <jsszsyc@hotmail.com>
2023-03-09 10:17:13 -0500

    Update

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -9,21 +9,35 @@ class Model(torch.nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
+        num_features = 30
         self.cnn = nn.Sequential(
             nn.Conv2d(
                 in_channels=num_channels,
-                out_channels=32,
+                out_channels=num_features,
                 kernel_size=3,
                 stride=2,
                 padding=0,
             ),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=32),
+            nn.BatchNorm2d(num_features=num_features),
             nn.Conv2d(
-                in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=0
+                in_channels=num_features,
+                out_channels=num_features,
+                kernel_size=3,
+                stride=2,
+                padding=0,
+            ),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=num_features),
+            nn.Conv2d(
+                in_channels=num_features,
+                out_channels=num_features,
+                kernel_size=3,
+                stride=2,
+                padding=0,
             ),
             nn.Flatten(),
-            nn.Linear(in_features=32 * 7 * 7, out_features=num_classes),
+            nn.Linear(in_features=num_features * 3 * 3, out_features=num_classes),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
