8e226c88ae62e8305f86ab127ebd24accc26dc62
YichengShen <jsszsyc@hotmail.com>
2023-03-10 09:19:46 -0500

    Update

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -37,8 +37,6 @@ class Model(torch.nn.Module):
                 stride=2,
                 padding=0,
             ),
-            # nn.Flatten(),
-            # nn.Linear(in_features=num_features, out_features=num_classes),
         )
         self.flat = nn.Flatten()
         self.fc = nn.Linear(in_features=num_features, out_features=num_classes)
@@ -48,7 +46,7 @@ class Model(torch.nn.Module):
         Forward.
         """
         x = self.cnn(x)
-        x = x * 1.04
+        x = x * 1.02
         x = self.flat(x)
         x = self.fc(x)
         return x
