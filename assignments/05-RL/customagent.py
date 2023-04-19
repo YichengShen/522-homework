cb1797fcc54f33ee9fa323dbe51885f5def4768c
YichengShen <jsszsyc@hotmail.com>
2023-04-19 14:54:30 -0400

    Update

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -5,172 +5,6 @@ import torch.nn as nn
 import torch.optim as optim
 from collections import deque
 import random
-from typing import Tuple, List
-
-
-class QNetwork(nn.Module):
-    """Deep Q-Network for approximating action-value function."""
-
-    def __init__(self, input_size, output_size, hidden_size=32):
-        """
-        Initialize the Q-Network.
-
-        Args:
-            input_size: The size of the input state.
-            output_size: The number of actions in the action space.
-            hidden_size: The size of the hidden layers (default: 32).
-        """
-        super(QNetwork, self).__init__()
-        self.network = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, output_size),
-        )
-
-    def forward(self, x) -> torch.Tensor:
-        """Forward pass to compute Q-values for the given state."""
-        return self.network(x)
-
-
-class ReplayBuffer:
-    """Experience Replay Buffer for storing and sampling experiences."""
-
-    def __init__(self, capacity):
-        """
-        Initialize the ReplayBuffer.
-
-        Args:
-            capacity: The maximum number of experiences to store in the buffer.
-        """
-        self.buffer = deque(maxlen=capacity)
-
-    def push(self, state, action, reward, next_state, done) -> None:
-        """
-        Add an experience to the buffer.
-
-        Args:
-            state: The current state.
-            action: The action taken.
-            reward: The reward received.
-            next_state: The next state.
-            done: Whether the episode has terminated.
-        """
-        self.buffer.append((state, action, reward, next_state, done))
-
-    def sample(self, batch_size) -> List[Tuple]:
-        """
-        Sample a batch of experiences from the buffer.
-
-        Args:
-            batch_size: The number of experiences to sample.
-
-        Returns:
-            A batch of experiences.
-        """
-        return random.sample(self.buffer, batch_size)
-
-    def __len__(self) -> int:
-        """Return the current size of the buffer."""
-        return len(self.buffer)
-
-
-class Agent:
-    """Deep Q-Learning Agent."""
-
-    def __init__(self, action_space: gym.Space, observation_space: gym.Space):
-        """
-        Initialize the DQNAgent.
-
-        Args:
-            action_space: The action space of the environment.
-            observation_space: The observation space of the environment.
-        """
-        self.action_space = action_space
-        self.observation_space = observation_space
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        # Hyperparameters
-        self.gamma = 0.99
-        self.epsilon_start = 1.0
-        self.epsilon_end = 0.01
-        self.epsilon_decay = 0.995
-        self.batch_size = 64
-        self.update_frequency = 8
-        self.target_update_frequency = 100
-
-        self.epsilon = self.epsilon_start
-        self.steps = 0
-
-        input_size = observation_space.shape[0]
-        output_size = action_space.n
-
-        self.q_network = QNetwork(input_size, output_size).to(self.device)
-        self.target_network = QNetwork(input_size, output_size).to(self.device)
-        self.target_network.load_state_dict(self.q_network.state_dict())
-        self.optimizer = optim.Adam(self.q_network.parameters(), lr=0.005)
-
-        self.replay_buffer = ReplayBuffer(5000)
-
-        self.last_state = None
-        self.last_action = None
-
-    def act(self, observation: gym.Space) -> gym.Space:
-        """
-        Select an action based on the current observation using an epsilon-greedy policy.
-
-        Args:
-            observation: The current state of the environment.
-
-        Returns:
-            The selected action.
-        """
-        if np.random.rand() < self.epsilon:
-            action = self.action_space.sample()
-        else:
-            with torch.no_grad():
-                state = torch.FloatTensor(observation).to(self.device)
-                q_values = self.q_network(state)
-                action = int(torch.argmax(q_values).item())
-
-        self.last_state = observation
-        self.last_action = action
-
-        return action
-
-    def learn(
-        self, observation: gym.Space, reward: float, terminated: bool, truncated: bool
-    ) -> None:
-        """
-        Learn from the given experience.
-
-        Args:
-            observation: The current state of the environment.
-            reward: The reward received.
-            terminated: Whether the episode has terminated.
-            truncated: Whether the episode was truncated.
-        """
-        if self.last_state is None or self.last_action is None:
-            return
-
-        self.steps += 1
-        self.replay_buffer.push(
-            self.last_state, self.last_action, reward, observation, terminated
-        )
-
-        if (
-            len(self.replay_buffer) < self.batch_size
-            or self.steps % self.update_frequency != 0
-        ):
-            return
-
-        batch = self.replay_buffer.sample(self.batch_size)
-        states, actions, rewards, next_states, dones = zip(*batch)
-
-        states = torch.FloatTensor(states).to(self.device)
-        actions = torch.LongTensor(actions).to(self.device)
-        rewards = torch.FloatTensor(rewards).to(self.device)
-        next_states = torch.FloatTensor(next_states).to(self.device)
-        dones = torch.FloatTensor(dones).to(self.device)
 
 
 class BaseAgent:
@@ -206,3 +40,144 @@ class BaseAgent:
         Learn from the given experience.
         """
         pass
+
+
+class DQNetwork(nn.Module):
+    """DQN"""
+
+    def __init__(self, input_size, output_size):
+        super(DQNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, output_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        return self.fc3(x)
+
+
+class Agent(BaseAgent):
+    """
+    A deep Q-network (DQN) agent that learns to perform actions based on observations using a neural network.
+    """
+
+    def __init__(
+        self,
+        action_space: gym.spaces.Discrete,
+        observation_space: gym.spaces.Box,
+        gamma: float = 0.99,
+        epsilon_start: float = 1.0,
+        epsilon_end: float = 0.01,
+        epsilon_decay: float = 0.995,
+        learning_rate: float = 0.001,
+        batch_size: int = 64,
+        memory_size: int = 10000,
+    ):
+        super().__init__(action_space, observation_space)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.gamma = gamma
+        self.epsilon = epsilon_start
+        self.epsilon_end = epsilon_end
+        self.epsilon_decay = epsilon_decay
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.memory = deque(maxlen=memory_size)
+
+        self.network = DQNetwork(
+            self.observation_space.shape[0], self.action_space.n
+        ).to(self.device)
+        self.target_network = DQNetwork(
+            self.observation_space.shape[0], self.action_space.n
+        ).to(self.device)
+        self.target_network.load_state_dict(self.network.state_dict())
+        self.target_network.eval()
+
+        self.optimizer = optim.Adam(self.network.parameters(), lr=self.learning_rate)
+        self.criterion = nn.MSELoss()
+
+        self.last_observation = None
+        self.last_action = None
+
+    def act(self, observation: gym.spaces.Box) -> gym.spaces.Discrete:
+        """
+        Select an action based on the current observation using the DQN.
+
+        Args:
+            observation: The current observation from the environment.
+
+        Returns:
+            action: The chosen action.
+        """
+        if np.random.rand() < self.epsilon:
+            return self.action_space.sample()
+        else:
+            observation = torch.tensor([observation], dtype=torch.float32).to(
+                self.device
+            )
+            with torch.no_grad():
+                return int(torch.argmax(self.network(observation)).item())
+
+    def learn(
+        self,
+        observation: gym.spaces.Box,
+        reward: float,
+        terminated: bool,
+        truncated: bool,
+    ) -> None:
+        """
+        Learn from the given experience using the DQN.
+
+        Args:
+            observation: The current observation from the environment.
+            reward: The reward received after performing the action.
+            terminated: Whether the episode has terminated.
+            truncated: Whether the episode has been truncated.
+        """
+        if len(self.memory) < self.batch_size:
+            return
+        self.memory.append(
+            (
+                self.last_observation,
+                self.last_action,
+                reward,
+                observation,
+                terminated or truncated,
+            )
+        )
+        self.last_observation = observation
+        self.last_action = self.act(observation)
+
+        if len(self.memory) < self.batch_size:
+            return
+
+        minibatch = random.sample(self.memory, self.batch_size)
+        observations, actions, rewards, next_observations, dones = zip(*minibatch)
+        observations = torch.tensor(np.stack(observations), dtype=torch.float32).to(
+            self.device
+        )
+        actions = torch.tensor(actions, dtype=torch.int64).unsqueeze(1).to(self.device)
+        rewards = (
+            torch.tensor(rewards, dtype=torch.float32).unsqueeze(1).to(self.device)
+        )
+        next_observations = torch.tensor(
+            np.stack(next_observations), dtype=torch.float32
+        ).to(self.device)
+        dones = torch.tensor(dones, dtype=torch.float32).unsqueeze(1).to(self.device)
+
+        q_values = self.network(observations).gather(1, actions)
+        next_q_values = (
+            self.target_network(next_observations).detach().max(1)[0].unsqueeze(1)
+        )
+        target_q_values = rewards + (self.gamma * next_q_values * (1 - dones))
+
+        loss = self.criterion(q_values, target_q_values)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        self.epsilon = max(self.epsilon_end, self.epsilon_decay * self.epsilon)
+
+        if terminated or truncated:
+            self.target_network.load_state_dict(self.network.state_dict())
