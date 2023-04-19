fe1adc3153c8ada5a84c4e95366f0f187996c984
YichengShen <jsszsyc@hotmail.com>
2023-04-19 12:58:14 -0400

    Update

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -195,148 +195,148 @@ class PolicyValueNetwork(nn.Module):
         return self.policy(x), self.value(x)
 
 
-class AAgent:
-    """RL Agent"""
+# class AAgent:
+#     """RL Agent"""
 
-    def __init__(
-        self,
-        action_space,
-        observation_space,
-        hidden_size=64,
-        lr=3e-4,
-        gamma=0.99,
-        gae_lambda=0.95,
-        ppo_clip=0.2,
-        ppo_epochs=4,
-        batch_size=64,
-    ):
-        self.action_space = action_space
-        self.observation_space = observation_space
-        self.internal_trajectory = []
-        self.last_state = None
-        self.last_action = None
+#     def __init__(
+#         self,
+#         action_space,
+#         observation_space,
+#         hidden_size=64,
+#         lr=3e-4,
+#         gamma=0.99,
+#         gae_lambda=0.95,
+#         ppo_clip=0.2,
+#         ppo_epochs=4,
+#         batch_size=64,
+#     ):
+#         self.action_space = action_space
+#         self.observation_space = observation_space
+#         self.internal_trajectory = []
+#         self.last_state = None
+#         self.last_action = None
 
-        self.gamma = gamma
-        self.gae_lambda = gae_lambda
-        self.ppo_clip = ppo_clip
-        self.ppo_epochs = ppo_epochs
-        self.batch_size = batch_size
+#         self.gamma = gamma
+#         self.gae_lambda = gae_lambda
+#         self.ppo_clip = ppo_clip
+#         self.ppo_epochs = ppo_epochs
+#         self.batch_size = batch_size
 
-        input_size = observation_space.shape[0]
-        output_size = action_space.n
+#         input_size = observation_space.shape[0]
+#         output_size = action_space.n
 
-        self.network = PolicyValueNetwork(input_size, output_size, hidden_size)
-        self.optimizer = optim.Adam(self.network.parameters(), lr=lr)
+#         self.network = PolicyValueNetwork(input_size, output_size, hidden_size)
+#         self.optimizer = optim.Adam(self.network.parameters(), lr=lr)
 
-    def choose_action(self, state):
-        state = torch.FloatTensor(state)
-        with torch.no_grad():
-            policy, _ = self.network(state)
-        action = np.random.choice(self.action_space.n, p=policy.numpy())
-        return action
+#     def choose_action(self, state):
+#         state = torch.FloatTensor(state)
+#         with torch.no_grad():
+#             policy, _ = self.network(state)
+#         action = np.random.choice(self.action_space.n, p=policy.numpy())
+#         return action
 
-    def compute_gae(self, rewards, dones, values):
-        advantages = np.zeros_like(rewards)
-        gae = 0
-        for t in reversed(range(len(rewards))):
-            delta = rewards[t] + self.gamma * values[t + 1] * (1 - dones[t]) - values[t]
-            gae = delta + self.gamma * self.gae_lambda * (1 - dones[t]) * gae
-            advantages[t] = gae
-        return advantages
+#     def compute_gae(self, rewards, dones, values):
+#         advantages = np.zeros_like(rewards)
+#         gae = 0
+#         for t in reversed(range(len(rewards))):
+#             delta = rewards[t] + self.gamma * values[t + 1] * (1 - dones[t]) - values[t]
+#             gae = delta + self.gamma * self.gae_lambda * (1 - dones[t]) * gae
+#             advantages[t] = gae
+#         return advantages
 
-    def generate_batches(self, states, actions, returns, advantages, batch_size):
-        indices = np.arange(len(states))
-        np.random.shuffle(indices)
-        for i in range(0, len(states), batch_size):
-            batch_indices = indices[i : i + batch_size]
-            yield states[batch_indices], actions[batch_indices], returns[
-                batch_indices
-            ], advantages[batch_indices]
+#     def generate_batches(self, states, actions, returns, advantages, batch_size):
+#         indices = np.arange(len(states))
+#         np.random.shuffle(indices)
+#         for i in range(0, len(states), batch_size):
+#             batch_indices = indices[i : i + batch_size]
+#             yield states[batch_indices], actions[batch_indices], returns[
+#                 batch_indices
+#             ], advantages[batch_indices]
 
-    def update(self, states, actions, returns, advantages):
-        states = torch.FloatTensor(states)
-        actions = torch.LongTensor(actions)
-        returns = torch.FloatTensor(returns)
-        advantages = torch.FloatTensor(advantages)
+#     def update(self, states, actions, returns, advantages):
+#         states = torch.FloatTensor(states)
+#         actions = torch.LongTensor(actions)
+#         returns = torch.FloatTensor(returns)
+#         advantages = torch.FloatTensor(advantages)
 
-        for _ in range(self.ppo_epochs):
-            for (
-                batch_states,
-                batch_actions,
-                batch_returns,
-                batch_advantages,
-            ) in self.generate_batches(
-                states, actions, returns, advantages, self.batch_size
-            ):
-                policy, values = self.network(batch_states)
-                action_probs = policy[np.arange(len(batch_actions)), batch_actions]
-                old_action_probs = action_probs.detach()
+#         for _ in range(self.ppo_epochs):
+#             for (
+#                 batch_states,
+#                 batch_actions,
+#                 batch_returns,
+#                 batch_advantages,
+#             ) in self.generate_batches(
+#                 states, actions, returns, advantages, self.batch_size
+#             ):
+#                 policy, values = self.network(batch_states)
+#                 action_probs = policy[np.arange(len(batch_actions)), batch_actions]
+#                 old_action_probs = action_probs.detach()
 
-                ratio = action_probs / old_action_probs
-                clip_advantages = (
-                    torch.clamp(ratio, 1 - self.ppo_clip, 1 + self.ppo_clip)
-                    * batch_advantages
-                )
-                loss_policy = -torch.min(
-                    ratio * batch_advantages, clip_advantages
-                ).mean()
+#                 ratio = action_probs / old_action_probs
+#                 clip_advantages = (
+#                     torch.clamp(ratio, 1 - self.ppo_clip, 1 + self.ppo_clip)
+#                     * batch_advantages
+#                 )
+#                 loss_policy = -torch.min(
+#                     ratio * batch_advantages, clip_advantages
+#                 ).mean()
 
-                loss_value = ((batch_returns - values.squeeze(-1)) ** 2).mean()
-                loss = loss_policy + loss_value
+#                 loss_value = ((batch_returns - values.squeeze(-1)) ** 2).mean()
+#                 loss = loss_policy + loss_value
 
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
+#                 self.optimizer.zero_grad()
+#                 loss.backward()
+#                 self.optimizer.step()
 
-    def act(self, observation: gym.spaces.Box) -> gym.spaces.Discrete:
-        """Act"""
-        state_tensor = torch.FloatTensor(observation).unsqueeze(0)
-        with torch.no_grad():
-            action_probs, _ = self.network(state_tensor)
-            action_dist = torch.distributions.Categorical(probs=action_probs)
-        return action_dist.sample().item()
+#     def act(self, observation: gym.spaces.Box) -> gym.spaces.Discrete:
+#         """Act"""
+#         state_tensor = torch.FloatTensor(observation).unsqueeze(0)
+#         with torch.no_grad():
+#             action_probs, _ = self.network(state_tensor)
+#             action_dist = torch.distributions.Categorical(probs=action_probs)
+#         return action_dist.sample().item()
 
-    def learn(
-        self,
-        observation: gym.spaces.Box,
-        reward: float,
-        terminated: bool,
-        truncated: bool,
-    ) -> None:
-        """Learn"""
-        if self.last_state is not None:
-            self.internal_trajectory.append(
-                (self.last_state, self.last_action, reward, observation, terminated)
-            )
+#     def learn(
+#         self,
+#         observation: gym.spaces.Box,
+#         reward: float,
+#         terminated: bool,
+#         truncated: bool,
+#     ) -> None:
+#         """Learn"""
+#         if self.last_state is not None:
+#             self.internal_trajectory.append(
+#                 (self.last_state, self.last_action, reward, observation, terminated)
+#             )
 
-        if terminated or truncated:
-            self._learn(self.internal_trajectory)
-            self.internal_trajectory = []
+#         if terminated or truncated:
+#             self._learn(self.internal_trajectory)
+#             self.internal_trajectory = []
 
-        self.last_state = observation
-        self.last_action = self.act(observation)
+#         self.last_state = observation
+#         self.last_action = self.act(observation)
 
-    def _learn(self, trajectory):
-        states, actions, rewards, next_states, dones = zip(*trajectory)
+#     def _learn(self, trajectory):
+#         states, actions, rewards, next_states, dones = zip(*trajectory)
 
-        states = np.array(states)
-        actions = np.array(actions)
-        rewards = np.array(rewards)
-        next_states = np.array(next_states)
-        dones = np.array(dones)
+#         states = np.array(states)
+#         actions = np.array(actions)
+#         rewards = np.array(rewards)
+#         next_states = np.array(next_states)
+#         dones = np.array(dones)
 
-        with torch.no_grad():
-            _, values = self.network(torch.FloatTensor(states))
-            _, next_values = self.network(torch.FloatTensor(next_states))
-        values = values.squeeze(-1).numpy()
-        next_values = next_values.squeeze(-1).numpy()
+#         with torch.no_grad():
+#             _, values = self.network(torch.FloatTensor(states))
+#             _, next_values = self.network(torch.FloatTensor(next_states))
+#         values = values.squeeze(-1).numpy()
+#         next_values = next_values.squeeze(-1).numpy()
 
-        advantages = self.compute_gae(
-            rewards, dones, np.concatenate((values, next_values[-1:]), axis=0)
-        )
-        returns = advantages + values
+#         advantages = self.compute_gae(
+#             rewards, dones, np.concatenate((values, next_values[-1:]), axis=0)
+#         )
+#         returns = advantages + values
 
-        self.update(states, actions, returns, advantages)
+#         self.update(states, actions, returns, advantages)
 
 
 class Agent:
