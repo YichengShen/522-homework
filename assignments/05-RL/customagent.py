043be697d80fb6316834c043f2ba664b0b823bb5
YichengShen <jsszsyc@hotmail.com>
2023-04-23 17:59:29 -0400

    Update

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -67,7 +67,7 @@ class Agent:
         time_step = curr_time - self.prev_time
         self.prev_time = curr_time
 
-        T = 485 / 1000
+        T = 486 / 1000
 
         angle_threshold = T * (1 - np.abs(observation[0]))
         target_angle = observation[0] * 0.6 + observation[2] * 1.0
